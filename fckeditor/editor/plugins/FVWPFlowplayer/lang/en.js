5e907637faa6ae9ae6f49772b2ed8e4e66d767a2
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 16:06:16 +0000

    0.9.8 version
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@206524 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fckeditor/editor/plugins/FVWPFlowplayer/lang/en.js
@@ -0,0 +1,21 @@
+﻿/*
+ * FCKeditor - The text editor for internet
+ * Copyright (C) 2003-2005 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ *    http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ *    http://www.fckeditor.net/
+ * 
+ * File Name: en.js
+ *  Abbr English language file.
+ * 
+ */
+FCKLang.Abbr	      		= 'Abbreviation' ;
+FCKLang.AbbrBtn      		= 'Insert abbreviation' ;
+FCKLang.AbbrDlgTitle 		= 'Add abbreviation-tag' ;
+FCKLang.AbbrDlgName  		= 'Please insert popup-text:' ;
+FCKLang.AbbrDlgTxt   		= '...Text gets visible while mouseover with cursor [<abbr title="Example-Pop-Up">Example...</abbr>]';
+FCKLang.AbbrDlgRemove		= 'Remove abbreviation-tag (content remains)';
+FCKLang.AbbrDlgSelectFirst	= 'You have to select text first!';
