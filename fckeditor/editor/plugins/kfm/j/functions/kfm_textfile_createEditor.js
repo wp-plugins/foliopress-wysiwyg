f3bfbdb21cf9c270ed49d02fe204c74e8bd0590e
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-06 10:49:43 +0000

    0.9.10 removing, as its not showin up
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@225712 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fckeditor/editor/plugins/kfm/j/functions/kfm_textfile_createEditor.js
+++
@@ -1,6 +0,0 @@
-window.kfm_textfile_createEditor=function(){
-	CodePress.run();
-	var tip=document.getElementById('kfm_tooltip');
-	if(tip)tip.parentNode.removeChild(tip);
-	kfm_textfile_attachKeyBinding();
-}
