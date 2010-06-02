78f8e4aa7adc3cf03d25f9ec4f383647a612ac49
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-02 13:44:39 +0000

    cleanup
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@248153 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fckeditor/editor/plugins/kfm/j/functions/kfm_textfile_attachKeyBinding.js
+++
@@ -1,6 +0,0 @@
-window.kfm_textfile_attachKeyBinding=function(){
-	if(!codepress.editor||!codepress.editor.body)return setTimeout('kfm_textfile_attachKeyBinding();',1);
-	var doc=codepress.contentWindow.document;
-	if(doc.attachEvent)doc.attachEvent('onkeypress',kfm_textfile_keybinding);
-	else doc.addEventListener('keypress',kfm_textfile_keybinding,false);
-}
