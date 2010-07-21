47e9aec79f16d706f9a8c2488378cdd9cf4c755c
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-21 17:20:50 +0000

    new version comming
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@265295 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fckeditor/editor/plugins/kfm/j/file.text-editing.js
+++
@@ -1,9 +0,0 @@
-// see ../license.txt for licensing
-llStubs.push('kfm_createEmptyFile');
-llStubs.push('kfm_leftColumn_disable');
-llStubs.push('kfm_leftColumn_enable');
-llStubs.push('kfm_textfile_attachKeyBinding');
-llStubs.push('kfm_textfile_close');
-llStubs.push('kfm_textfile_createEditor');
-llStubs.push('kfm_textfile_initEditor');
-llStubs.push('kfm_textfile_keybinding');
