fc7b08a6521a42c6c0a696c23284dcfc091bd6a0
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-19 18:54:48 +0000

    preparing for 0.9.19.5
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@412139 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fckeditor/editor/plugins/kfm/includes/json.php
+++
@@ -1,9 +0,0 @@
-<?php
-function kfm_json_encode($obj){
-	$json=new kfm_Services_JSON();
-	return $json->encode($obj);
-}
-function kfm_json_decode($js){
-	$json=new kfm_Services_JSON();
-	return $json->decode($js);
-}
--- view/message.php
+++
@@ -1,3 +0,0 @@
-<div class="wrap">
-	<?php echo $strMessage; ?>
-</div>
