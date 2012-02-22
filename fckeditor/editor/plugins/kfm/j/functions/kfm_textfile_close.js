fb44e4370bf32d42655a365772ba0c6b5438ebc1
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-22 10:04:03 +0000

    Cleanup for new version
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@508764 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fckeditor/editor/plugins/kfm/j/functions/kfm_textfile_close.js
+++
@@ -1,6 +0,0 @@
-window.kfm_textfile_close=function(){
-	if(document.getElementById("edit-start").value!=codepress.getCode() && !kfm.confirm( kfm.lang.CloseWithoutSavingQuestion))return;
-	kfm_leftColumn_enable();
-	kfm_changeDirectory("kfm_directory_icon_"+kfm_cwd_id);
-	$j.event.remove(document.getElementById('kfm_right_column'),'keyup',kfm_textfile_keybinding);
-}
--- fckeditor/editor/plugins/kfm/j/functions/kfm_textfile_keybinding.js
+++
@@ -1,5 +0,0 @@
-window.kfm_textfile_keybinding=function(e){
-	if(e.which!=27)return;
-	e.stopPropagation();
-	kfm_textfile_close();
-}
