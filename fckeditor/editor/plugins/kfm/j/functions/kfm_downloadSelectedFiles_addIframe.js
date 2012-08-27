54a8575af6fc3d8a77ae8300ba015edaebc7a3f2
FolioVision <FolioVision@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-27 15:42:12 +0000

    clean up
    git-svn-id: https://plugins.svn.wordpress.org/foliopress-wysiwyg/trunk@591002 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fckeditor/editor/plugins/kfm/j/functions/kfm_downloadSelectedFiles_addIframe.js
+++
@@ -1,5 +0,0 @@
-window.kfm_downloadSelectedFiles_addIframe=function(wrapper,id){
-	var iframe=document.createElement('iframe');
-	iframe.src='getfile.php?id='+id+'&forcedownload=1';
-	kfm.addEl(wrapper,iframe);
-}
